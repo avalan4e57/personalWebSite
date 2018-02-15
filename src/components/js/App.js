06f2a18cace052e1202f3fab08706262989f5a5e
Igor Chernega <am.igor.chernega@gmail.com>
2018-02-15 14:18:09 +0200

    Move cards state from App to Projects component

--- src/components/js/App.js
+++ src/components/js/App.js
@@ -8,80 +8,8 @@ import Home from './Home.js'
 import Projects from './Projects.js'
 import Header from './Header.js'
 import Navigation from './Navigation.js'
-import data from './../../projectsData.json'
 
 class App extends Component {
-  constructor() {
-    super()
-    this.state = {
-      cards: [],
-      queryInput: ''
-    }
-
-    this.toggleSelect = this.toggleSelect.bind(this)
-    this.execQuery = this.execQuery.bind(this)
-    this.readQuery = this.readQuery.bind(this)
-  }
-
-  componentDidMount() {
-    // fetch('https://igor-chernega.herokuapp.com/assets/projectsData.json')
-    //   .then(resp => resp.json())
-    //   .then(data => {
-    //     let cards = data.map(item => ({
-    //         id: item.id,
-    //         image: item.image,
-    //         name: item.name,
-    //         description: item.description,
-    //         github: item.github,
-    //         www: item.www
-    //       })
-    //     )
-    //     this.setState({ cards: cards })
-    //   })
-    let cards = data.map(item => ({
-        id: item.id,
-        image: item.image,
-        name: item.name,
-        description: item.description,
-        github: item.github,
-        www: item.www
-      })
-    )
-    this.setState({ cards: cards })
-    console.log(cards[0])
-  }
-
-  toggleSelect(id) {
-    this.setState({ cards: this.state.cards.map(card =>
-      {
-        if (card.id == id) {
-          card.selected = !card.selected
-        }
-        return card
-      }
-    ) })
-  }
-
-  execQuery() {
-    fetch('https://api.punkapi.com/v2/beers/' + this.state.queryInput)
-      .then(resp => resp.json())
-      .then(data => {
-        let cards = data.map(item => ({
-            id: item.id,
-            image: item.image_url,
-            name: item.name,
-            description: item.description,
-            selected: false
-          })
-        )
-        this.setState({ cards: cards })
-      })
-  }
-
-  readQuery(e) {
-    this.setState({ queryInput: e.target.value })
-  }
-
   render() {
     return(
       <Router>
@@ -90,7 +18,7 @@ class App extends Component {
           <Navigation />
 
           <Route exact path='/' render={ () => <Home /> } />
-          <Route path='/projects' render={ () => <Projects cards={ this.state.cards } /> } />
+          <Route path='/projects' render={ () => <Projects /> } />
         </div>
       </Router>
     )
--- src/components/js/Projects.js
+++ src/components/js/Projects.js
@@ -1,12 +1,34 @@
 import React, { Component } from 'react'
 import Main from './Main.js'
+import data from './../../projectsData.json'
 
 class Projects extends Component {
+  constructor() {
+    super()
+    this.state = {
+      cards: [],
+      queryInput: ''
+    }
+  }
+
+  componentDidMount() {
+    let cards = data.map(item => ({
+        id: item.id,
+        image: item.image,
+        name: item.name,
+        description: item.description,
+        github: item.github,
+        www: item.www
+      })
+    )
+    this.setState({ cards: cards })
+  }
+
   render() {
     return(
       <div>
         <h1>My pet projects</h1>
-        <Main cards={ this.props.cards } />
+        <Main ccards={ this.state.cards } />
       </div>
     )
   }
